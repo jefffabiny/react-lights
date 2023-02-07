6fcaf6eb3f03ea58498b540c75dc152158312bdf
Jeff Fabiny <jefffabiny@gmail.com>
2023-02-06 23:20:14 -0500

    More fleshed out lights.

--- src/components/SVG.tsx
+++ src/components/SVG.tsx
@@ -1,15 +1,10 @@
 function Svg() {
   return (
     <>
-      <svg
-        width="100%"
-        height="100%"
-        style={{ border: "1px solid blue" }}
-        viewBox={"-100 -100 200 200"}
-      >
+      <svg width="100%" height="100%" viewBox={"-100 -100 200 200"}>
         <defs>
           <g id="bulb">
-            <path d="M 0,0 Q 20 25 0 40 Q -20 25 0 0" />
+            <path d="M 0,0 Q 20 25 0 40 Q -20 25 0 0" fill="#f0f0f0" />
             <rect x="-6" y="0" width="12" height="10" rx="3" fill="#343434" />
           </g>
         </defs>
@@ -18,7 +13,13 @@ function Svg() {
           fill="none"
           stroke="#343434"
         />
-        <use href="#bulb" x="20" y="-37" fill="blue" transform="rotate(-28)" />
+        <use href="#bulb" x="-130" y="79" transform="rotate(55)" />
+        <use href="#bulb" x="-110" y="11" transform="rotate(23)" />
+        <use href="#bulb" x="-50" y="-34" transform="rotate(-8)" />
+        <use href="#bulb" x="20" y="-37" transform="rotate(-28)" />
+        <use href="#bulb" x="62" y="-52" transform="rotate(-12)" />
+        <use href="#bulb" x="60" y="-111" transform="rotate(39)" />
+        <use href="#bulb" x="80" y="-127" transform="rotate(53)" />
       </svg>
     </>
   );
